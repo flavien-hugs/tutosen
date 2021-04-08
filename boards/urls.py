510a6651b2b42b23178d713345d9f9cf9a10c8c9
flavien-hugs <geekactux@gmail.com>
2021-04-08 20:50:25 +0000

    update dashbord design

--- boards/urls.py
+++ boards/urls.py
@@ -7,7 +7,13 @@ from django.views import generic
 
 app_name = 'boards'
 urlpatterns = [
-    path('', generic.TemplateView.as_view(
+    path('instructor/', generic.TemplateView.as_view(
         template_name='dashboard/teacher/teacher_dashboard.html',
-        extra_context={'page_title': 'Tableau de bord Instructeur'}), name='teacher_dashboard_url'),
+        extra_context={'page_title': 'Tableau de bord Instructeur'}),
+        name='teacher_dashboard_url'),
+
+    path('cours/add/', generic.TemplateView.as_view(
+        template_name='dashboard/teacher/teacher_add_course.html',
+        extra_context={'page_title': 'Ajouter un nouveau cours'}),
+        name='teacher_add_course_url'),
 ]
