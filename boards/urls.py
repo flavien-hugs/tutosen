077989b5b0acbe5af4600faa000004aa713cc760
flavien-hugs <geekactux@gmail.com>
2021-04-08 08:27:41 +0000

    rename apps dashboard into boards

---
+++ boards/urls.py
@@ -0,0 +1,13 @@
+# boards.urls.py
+
+from django.urls import path
+
+from django.views import generic
+
+
+app_name = 'boards'
+urlpatterns = [
+    path('', generic.TemplateView.as_view(
+        template_name='dashboard/teacher/teacher_dashboard.html',
+        extra_context={'page_title': 'Tableau de bord Instructeur'}), name='teacher_dashboard_url'),
+]
