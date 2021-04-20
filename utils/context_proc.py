6ccfdf4b4951a66ff862162d750521483b5d3d41
flavien-hugs <geekactux@gmail.com>
2021-04-20 19:38:52 +0000

    add contact website

--- utils/context_proc.py
+++ utils/context_proc.py
@@ -7,6 +7,7 @@ def tutosen_context_processor(request):
     return {
         'title': settings.SITE_NAME,
         'addr_email': 'support@tutosen.com',
+        'addr_contact': '01 51 57 13 96',
         'site_description': settings.SITE_DESCRIPTION,
         'site_keywords': settings.META_KEYWORDS,
         'request': request,
