b6067e92d3a6c50fafac03fade13a84ae138dcd5
flavien-hugs <geekactux@gmail.com>
2021-06-20 11:44:46 +0000

    add accounts personnal command

---
+++ accounts/management/commands/reset_accounts_db.py
@@ -0,0 +1,13 @@
+# accounts.management.commands.reset_users.py
+
+from django.core.management.base import BaseCommand
+
+from ...models import Teacher, Student, User
+
+
+class Command(BaseCommand):
+    help = "Reset the database"
+
+    def handle(self, *args, **options):
+        User.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS("Réinitialisation des utilisateurs"))
---
+++ accounts/management/commands/seed_db.py
@@ -0,0 +1,19 @@
+# accounts/management/commands/seed_py.py
+
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+from accounts.models import Teacher
+
+
+class Command(BaseCommand):
+    help = "Seeds the database with 10 users"
+    
+    def handle(self, *args, **options):
+        fake = Faker()
+        # remove existing data
+        Teacher.objects.all().delete()
+        # add 10 users
+        users = [Teacher(last_name=fake.last_name()) for index in range(10)]
+        Teacher.objects.bulk_create(users)
+        print(f"users created.")
