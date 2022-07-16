6f7e508da2655bf30642d970e78c23ec47d487bc
flavien-hugs <geekactux@gmail.com>
2022-07-16 21:46:37 +0000

    improve whitenoise config

--- core/production.py
+++ core/production.py
@@ -17,20 +17,3 @@ ALLOWED_HOSTS = [
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-# https://docs.djangoproject.com/fr/3.0/ref/settings/
-# Let's Encrypt ssl/tls https
-
-X_FRAME_OPTIONS = "DENY"
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_SECONDS = 86400
-SECURE_HSTS_PRELOAD = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-SESSION_COOKIE_SECURE = True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-
-CSRF_USE_SESSIONS = True
-CSRF_COOKIE_SECURE = True
