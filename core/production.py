1a0297ab0595ee575e9956ecc14c4adf66ac8d77
flavien-hugs <geekactux@gmail.com>
2022-07-16 21:22:45 +0000

    change debug

--- core/production.py
+++ core/production.py
@@ -5,7 +5,7 @@ from core.settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production
 
-DEBUG = TEMPLATE_DEBUG = True
+DEBUG = TEMPLATE_DEBUG = False
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 
