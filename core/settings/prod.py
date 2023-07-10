361575ecdd1801d9469797f7f754c7419b618169
haf <haf@incubtek.com>
2023-07-10 16:37:48 +0000

    update allowed hosts

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -9,10 +9,7 @@ BASE_DIR = os.path.dirname(dirname)
 
 DEBUG = TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = [
-    'unstaschool.herokuapp.com',
-    '*.herokuapp.com'
-]
+ALLOWED_HOSTS = ["*"]
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
